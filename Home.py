c467ba6c379ea6530f2e68ce4921873365861ec4
chinmay palshetkar <chinmaypalshetkar952@gmail.com>
2025-08-02 18:42:16 +0530

    Updated all Python apps with latest changes

--- Home.py
+++ Home.py
@@ -2,6 +2,12 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 st.title("📊 UK Traffic Dashboard")
+(st.markdown("""
+---
+**Created by:** Chinmay Palshetkar  
+[LinkedIn Profile](https://www.linkedin.com/in/chinmay-palshetkar-923164291)
+---
+"""))
 
 st.markdown("""
 ### Welcome!
